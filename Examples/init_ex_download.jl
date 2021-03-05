119ec5a2a685144b959d35eed77dd9ff385a0927
kura-okubo <kura.okubo@gmail.com>
2021-03-05 17:43:09 +0900

    update ex download

---
+++ Examples/init_ex_download.jl
@@ -0,0 +1,43 @@
+using SeisMonitoring
+
+# This script initiate project
+project_name = "ex_download_data"
+project_inputdir="./"
+project_outputdir="./"
+
+# project_outputdir="/scratch1/07208/kokubo09/BP_v25-28"
+
+master_param="./mainparam_master.jl"
+
+# make default input and output directory
+init_project(project_name=project_name,
+             project_inputdir=project_inputdir, # local directory
+             project_outputdir=project_outputdir,
+             force=true, gui=false # external HDD
+            )
+
+
+# make request station list
+fipath = "./BP_gmap-stations.txt"
+
+locchan = Dict(
+            "BP" => [("*", "BP*"), ("*", "SP*")]
+            )
+
+station_fodir = project_outputdir*"/$(project_name)_OUTPUT"
+station_path = make_requeststation_fromIRISgmap(fipath, locchan=locchan, fodir=station_fodir, foname="$(project_name).jld2")
+
+
+# copy master parameter file into project directory
+fo_mainparam = project_inputdir*"/$(project_name)_INPUT/mainparam.jl"
+cp(master_param, fo_mainparam)
+
+# replace parameters for casestudy
+set_parameter(fo_mainparam, "project_name", project_name)
+set_parameter(fo_mainparam, "project_inputdir", project_inputdir*"/$(project_name)_INPUT")
+set_parameter(fo_mainparam, "project_outputdir", project_outputdir*"/$(project_name)_OUTPUT")
+set_parameter(fo_mainparam, "requeststation_file",  station_fodir*"/$(project_name).jld2")
+# the rest of parameters should be modified case by case.
+
+
+

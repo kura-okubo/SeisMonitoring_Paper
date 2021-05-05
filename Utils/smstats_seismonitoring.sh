a5ddaa6810c5125a8b15082943fb59979be6f7f7
Kurama Okubo <kokubo09@login2.frontera.tacc.utexas.edu>
2021-05-04 20:58:03 -0500

    update without remove EQ

--- Utils/smstats_seismonitoring.sh
+++ Utils/smstats_seismonitoring.sh
@@ -2,11 +2,13 @@
 np=48
 
 #$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_raw_2010-2020_stretching_OUTPUT/stack/shorttime ../OUTPUT stretching monitoring_stats_2010-2020_stretching_OUTPUT.csv
-#$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_raw_2010-2020_mwcs_OUTPUT/stack/shorttime ../OUTPUT mwcs monitoring_stats_2010-2020_mwcs_OUTPUT.csv
+$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_raw_2010-2020_mwcs_OUTPUT/stack/shorttime ../OUTPUT mwcs monitoring_stats_2010-2020_mwcs_OUTPUTi_cclencorrected.csv
 #$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_normalized_2010-2020_stretching_OUTPUT/stack/shorttime ../OUTPUT stretching monitoring_stats_normalized_2010-2020_stretching_OUTPUT.csv
 
-$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_normalized_2010-2020_mwcs_OUTPUT/stack/shorttime ../OUTPUT mwcs monitoring_stats_normalized_2010-2020_mwcs_OUTPUT.csv
+#$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_normalized_2010-2020_mwcs_OUTPUT/stack/shorttime ../OUTPUT mwcs monitoring_stats_normalized_2010-2020_mwcs_OUTPUT.csv
 
-$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_raw_robust_2010-2020_stretching_OUTPUT/stack/shorttime ../OUTPUT stretching monitoring_stats_raw_robust_2010-2020_stretching_OUTPUT.csv
+#$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_raw_robust_2010-2020_stretching_OUTPUT/stack/shorttime ../OUTPUT stretching monitoring_stats_raw_robust_2010-2020_stretching_OUTPUT.csv
+
+#$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_raw_withoutremovalEQ_2010-2020_stretching_OUTPUT/stack/shorttime ../OUTPUT stretching monitoring_stats_raw_withoutremovalEQ_2010-2020_stretching_OUTPUT.csv
 
 #$HOME/packages/julia-1.5.3/bin/julia -p $np run_smstats.jl /home1/07208/kokubo09/scratch/SeisMonitoring_Paper/ex_stack_raw_2006-2016_stretching_OUTPUT/stack/shorttime ../OUTPUT monitoring_stats_2006-2016_stretching_OUTPUT.csv

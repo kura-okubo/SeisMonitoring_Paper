f9d9a470beaca229b72ba7980b1ef4af3b3622af
kura-okubo <kura.okubo@gmail.com>
2023-08-18 17:06:12 -0700

    update mwcs uwbackup

---
+++ Examples/ex_stack_raw_2010-2022_mwcs_uwbackup_INPUT/runner_uwdata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+timestamp=$(date +PDT-%Y-%m-%d-%H-%M-%S)
+NPROCS=8
+
+date > log_$timestamp.txt
+/home/kokubo/packages/julia-1.7.3/bin/julia /home/kokubo/project/Parkfield_BP/SeisMonitoring_Paper/Examples/ex_stack_raw_2010-2022_mwcs_uwbackup_INPUT/topo_multi_slurm_run_uwdata.jl $NPROCS 2>&1 | tee -a log_$timestamp.txt
+date >> log_$timestamp.txt 

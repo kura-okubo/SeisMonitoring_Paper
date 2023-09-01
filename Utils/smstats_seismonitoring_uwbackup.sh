772a489212c697d7431c5fe8c8a55a6b147a1cf1
kura-okubo <kura.okubo@gmail.com>
2023-09-01 18:11:46 +0900

    update Utils

--- Utils/smstats_seismonitoring_uwbackup.sh
+++ Utils/smstats_seismonitoring_uwbackup.sh
@@ -2,4 +2,4 @@
 np=8
 
 # run smstats to compile csv files
-/home/kokubo/packages/julia-1.7.3/bin/julia -p $np run_smstats.jl /data/wsd04/Parkfield/ex_stack_raw_2010-2022_stretching_uwbackup_OUTPUT/stack/shorttime ../OUTPUT stretching monitoring_stats_uwbackup_2010-2022_stretching.csv
+/home/kokubo/packages/julia-1.7.3/bin/julia -p $np run_smstats.jl ex_stack_raw_2010-2022_stretching_uwbackup_OUTPUT/stack/shorttime ../OUTPUT stretching monitoring_stats_uwbackup_2010-2022_stretching.csv

b3eac24a9a2c0b945b843c46dee70159c64e5f14
kura-okubo <kura.okubo@gmail.com>
2024-01-11 18:18:08 +0900

    update depth sensitivity kernel

--- Appx/BP_sensitivity_kernel/code/runit_depthsensitivity_Parkfield.sh
+++ Appx/BP_sensitivity_kernel/code/runit_depthsensitivity_Parkfield.sh
@@ -7,15 +7,19 @@ echo ${dt}
 rm sdisp96*
 rm SRDER.PLT
 rm SRDER.TXT
-sprep96 -M model_Parkfield1D_CVM.dat -DT ${dt} -NPTS 2 -R -NMOD 1
+sprep96 -M model_Parkfield1D_CVM.dat -DT ${dt} -NPTS 2 -L -R -NMOD 1
 sdisp96 -v
 # HS and HR are dummy values
+slegn96 -HS 0 -HR 0 -DER -NOQ
 sregn96 -HS 0 -HR 0 -DER -NOQ
 sdpder96 -R -TXT -K 2 -XLEN 3.0 -X0 1.0 -YLEN 4.0
 mv SRDER.TXT SRDER_DT${dt}.TXT
+sdpder96 -L -TXT -K 2 -XLEN 3.0 -X0 1.0 -YLEN 4.0
+mv SLDER.TXT SLDER_DT${dt}.TXT
 done
 
-plotxvig < SRDER.PLT
+plotxvig < SLDER.PLT
+# plotxvig < SRDER.PLT
 
 # for i in 
 # > do
